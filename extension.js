29f7889e768aaf47b112723010f3b9c1e92d59e5
Yichao Yu <yyc1992@gmail.com>
2021-12-30 19:24:08 -0500

    Add server URL configure key
    Remove hello world command

--- extension.js
+++ extension.js
@@ -9,22 +9,6 @@ const vscode = require('vscode');
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "digitaljs" is now active!');
-
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('digitaljs.helloWorld', function () {
-        // The code you place here will be executed every time your command is executed
-
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World from DigitalJS!');
-    });
-
-    context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated

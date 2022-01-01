bd0a3fe6efcc37bdfe3418bfcb665c3adfec5004
Yichao Yu <yyc1992@gmail.com>
2022-01-01 07:28:14 -0500

    Fix style sheet

--- view/main.js
+++ view/main.js
@@ -6,5 +6,6 @@ import './scss/app.scss';
 import $ from 'jquery';
 import * as digitaljs from 'digitaljs';
 import * as digitaljs_lua from 'digitaljs_lua';
+import Split from 'split-grid';
 
 const vscode = window.acquireVsCodeApi();

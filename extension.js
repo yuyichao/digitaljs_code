cfe16d32ac39923beea092728f083fa4c2b73d78
Yichao Yu <yyc1992@gmail.com>
2022-01-07 08:30:24 -0500

    Clean up

--- extension.js
+++ extension.js
@@ -2,8 +2,6 @@
 
 'use strict';
 
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
 const path = require('path');
 const requests = require('./src/requests.js');
@@ -13,17 +11,10 @@ function hash_sha512(data) {
     return crypto.createHash('sha512').update(data).digest('hex');
 }
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-
-/**
- * @param {vscode.ExtensionContext} context
- */
 function activate(context) {
     new DigitalJS(context);
 }
 
-// this method is called when your extension is deactivated
 function deactivate() {
 }
 

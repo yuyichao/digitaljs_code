7126eeffa2c98d02d550d9acccd2853987fb8fbf
Yichao Yu <yyc1992@gmail.com>
2022-01-09 16:52:55 -0500

    Allow setting options in synth view externally

--- view/synth_view.mjs
+++ view/synth_view.mjs
@@ -6,19 +6,24 @@ const vscode = acquireVsCodeApi();
 
 class Synth {
     #options
+    #widgets
+    #block_update = false
     constructor(options) {
         this.#options = options;
-        window.addEventListener("load", () => {
-            this.#initialize();
-        });
+        window.addEventListener('message', event => this.#processMessage(event));
+        window.addEventListener("load", () => this.#initialize());
     }
     #notifyOption() {
         vscode.setState(this.#options);
+        if (this.#block_update)
+            return;
         vscode.postMessage({ command: "update-options", options: this.#options });
     }
     #initialize() {
+        this.#widgets = {};
         for (const opt of ['opt', 'transform', /* 'lint', */ 'fsmexpand']) {
             const ele = document.getElementById(opt);
+            this.#widgets[opt] = ele;
             ele.checked = this.#options[opt];
             ele.addEventListener('change', () => {
                 this.#options[opt] = ele.checked;
@@ -27,6 +32,7 @@ class Synth {
         }
 
         const fsm = document.getElementById("fsm");
+        this.#widgets.fsm = fsm;
         fsm.value = this.#options.fsm;
         fsm.addEventListener('change', () => {
             this.#options.fsm = fsm.value;
@@ -40,6 +46,26 @@ class Synth {
 
         vscode.postMessage({ command: 'initialized' });
         this.#notifyOption();
+    }
+    #setOptions(options) {
+        this.#block_update = true;
+        this.#options = options;
+        try {
+            for (const opt of ['opt', 'transform', /* 'lint', */ 'fsmexpand'])
+                this.#widgets[opt].checked = this.#options[opt];
+            this.#widgets.fsm.value = this.#options.fsm
+        }
+        finally {
+            this.#block_update = false;
+        }
+    }
+    #processMessage(event) {
+        const message = event.data;
+        switch (message.command) {
+            case 'update-options':
+                this.#setOptions(message.options);
+                return;
+        }
     }
 }
 

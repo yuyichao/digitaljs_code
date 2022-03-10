6b5521d2464da4674340ec5433cdb6eddecedabb
Yichao Yu <yyc1992@gmail.com>
2022-03-09 21:17:21 -0500

    Try prepending return in Lua REPL to match the commandline REPL behavior

--- view/main.mjs
+++ view/main.mjs
@@ -264,7 +264,7 @@ class LuaRunner {
         const runner = this.#getRunner(name, isrepl);
         let pid;
         try {
-            pid = runner.runThread(script, { print_result: isrepl });
+            pid = runner.runThread(script, { print_result: isrepl, prepend_return: isrepl });
             runner.running_pid = pid;
         }
         catch (e) {

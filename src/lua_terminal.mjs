48936793c14b17b7af8438c3e8839f4ef77a6395
Yichao Yu <yyc1992@gmail.com>
2022-03-12 22:40:29 -0500

    Automatically handle multi line input

--- src/lua_terminal.mjs
+++ src/lua_terminal.mjs
@@ -3,6 +3,41 @@
 'use strict';
 
 import REPL from './repl.mjs';
+import * as luaparse from 'luaparse';
+
+const unexpected_eof_suffix = (() => {
+    // This is basically how lua.c detect parser error...
+    // It would be nice if luaparse could give us a simpler ID to match...
+    // It's good enough for now and I don't really want to monkey patch luaparse ATM.
+    const err = luaparse.errors.unexpectedEOF;
+    return err.substring(err.length - 7);
+})();
+
+class ParseResult {
+    static OK = 1
+    static EOF = 2
+    static ERROR = 3
+}
+
+function try_parse(str) {
+    try {
+        luaparse.parse(str);
+    }
+    catch (e) {
+        if (e.message && e.message.endsWith(unexpected_eof_suffix))
+            return ParseResult.EOF;
+        return ParseResult.ERROR;
+    }
+    return ParseResult.OK;
+}
+
+function try_parse_with_return(str) {
+    // Do not append `;` here since we want the string to be at the end of the input.
+    const res = try_parse(`return ${str}`);
+    if (res === ParseResult.OK)
+        return res;
+    return Math.min(res, try_parse(str));
+}
 
 export class LuaTerminal extends REPL {
     #view
@@ -28,8 +63,8 @@ export class LuaTerminal extends REPL {
     }
 
     #try_submit(data) {
-        // TODO:
-        // * automatically detect if we should take more input lines
+        if (try_parse_with_return(data) === ParseResult.EOF)
+            return false;
         this.#view.post({
             command: 'runlua',
             name: `REPL[${this.#id++}]`,
